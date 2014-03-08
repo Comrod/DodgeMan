35801360d44579ee2d1ee85af5ae3aa094a61977
Comrod <icomrod@gmail.com>
2014-03-08 09:20:47 -0600

    Coder Dojo

---
+++ CoderDojo/CoderDojo/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  CoderDojo
+//
+
+//  Copyright (c) 2014 Testman Industries. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
