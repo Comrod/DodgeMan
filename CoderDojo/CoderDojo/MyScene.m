04c4a2fbd4d4052de8063e40076eaf843c35f858
Comrod <icomrod@gmail.com>
2014-03-08 16:32:12 -0600

    Revert 850ba50..2d1b1cc
    This rolls back to commit 850ba5089c5b768fff2542329afbcce0c0bbc54f.

---
+++ CoderDojo/CoderDojo/MyScene.m
@@ -0,0 +1,223 @@
+//
+//  MyScene.m
+//  CoderDojo
+//
+//  Created by Cormac Chester on 3/8/14.
+//  Copyright (c) 2014 Testman Industries. All rights reserved.
+//
+
+#import "MyScene.h"
+#import "EndGameScene.h"
+
+static const uint32_t redBallCategory =  0x1 << 0;
+static const uint32_t playerCategory =  0x1 << 1;
+
+@implementation MyScene
+
+-(id)initWithSize:(CGSize)size
+{
+    if (self = [super initWithSize:size])
+    {
+        /* Setup your scene here */
+        
+        //Sets player location
+        playerLocX = 50;
+        playerLocY = 89;
+        
+        //Sets player score
+        score = 0;
+        
+        //Set Background
+        SKSpriteNode *background = [SKSpriteNode spriteNodeWithImageNamed:@"backgroundiP5"];
+        background.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));
+        background.xScale = 0.5;
+        background.yScale = 0.5;
+        [self addChild:background];
+        
+        //Player
+        self.playerSprite = [SKSpriteNode spriteNodeWithImageNamed:@"character"];
+        self.playerSprite.position = CGPointMake(playerLocX, playerLocY);
+        [self addChild:self.playerSprite];
+        
+        self.scoreLabel = [SKLabelNode labelNodeWithFontNamed:@"Arial-BoldMT"];
+        self.scoreLabel.text = @"0";
+        self.scoreLabel.fontSize = 40;
+        self.scoreLabel.fontColor = [SKColor blackColor];
+        self.scoreLabel.position = CGPointMake(50, 260);
+        [self addChild:self.scoreLabel];
+        
+        //Pause Button
+        self.pauseButton = [SKSpriteNode spriteNodeWithImageNamed:@"pauseButton"];
+        self.pauseButton.position = CGPointMake(self.frame.size.width / 2, self.frame.size.height - 40);
+        self.pauseButton.name = @"pauseButton";
+        //[self addChild:self.pauseButton];
+        
+        //Set Player Physics
+        self.playerSprite.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:self.playerSprite.size];
+        self.playerSprite.physicsBody.dynamic = YES;
+        self.playerSprite.physicsBody.categoryBitMask = playerCategory;
+        self.playerSprite.physicsBody.contactTestBitMask = redBallCategory;
+        self.playerSprite.physicsBody.collisionBitMask = 0;
+        self.playerSprite.physicsBody.usesPreciseCollisionDetection = YES;
+        
+        //Prevents spazzing
+        self.physicsWorld.gravity = CGVectorMake(0,0);
+        self.physicsWorld.contactDelegate = self;
+        
+    }
+    return self;
+}
+
+-(void)addBall
+{
+    SKSpriteNode *redBall = [SKSpriteNode spriteNodeWithImageNamed:@"locationIndicator"];
+    int minY = redBall.size.height / 2;
+    int maxY = self.frame.size.height - redBall.size.height / 2;
+    int rangeY = maxY - minY;
+    int actualY = (arc4random() % rangeY) + minY;
+    NSLog(@"Actual Y: %i", actualY);
+    
+    //Initiates red ball offscreen
+    if (actualY >= 75)
+    {
+        //Prevents balls from spawning in the ground
+        redBall.position = CGPointMake(self.frame.size.width + redBall.size.width/2, actualY);
+        [self addChild:redBall];
+    }
+    redBall.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:redBall.size.width/2];
+    redBall.physicsBody.dynamic = YES;
+    redBall.physicsBody.categoryBitMask = redBallCategory;
+    redBall.physicsBody.contactTestBitMask = playerCategory;
+    redBall.physicsBody.collisionBitMask = 0;
+    
+    //Determine speed of red ball
+    int minDuration = 3.0;
+    int maxDuration = 5.0;
+    int rangeDuration = maxDuration - minDuration;
+    int actualDuration = (arc4random() % rangeDuration) + minDuration;
+    
+    // Create the actions
+    SKAction *actionMove = [SKAction moveTo:CGPointMake(-redBall.size.width/2, actualY) duration:actualDuration];
+    SKAction *actionMoveDone = [SKAction removeFromParent];
+    SKAction *ballCross = [SKAction runBlock:^{
+        score++;
+        self.scoreString = [NSString stringWithFormat:@"%i", score];
+        self.scoreLabel.text = self.scoreString;
+        NSLog(@"Score was incremented. Score is now %d", score);
+    }];
+    [redBall runAction:[SKAction sequence:@[actionMove, ballCross, actionMoveDone]]];
+}
+
+- (void)updateWithTimeSinceLastUpdate:(CFTimeInterval)timeSinceLast
+{
+    self.lastSpawnTimeInterval += timeSinceLast;
+    if (self.lastSpawnTimeInterval > 0.5) {
+        self.lastSpawnTimeInterval = 0;
+        [self addBall];
+    }
+}
+
+-(void)update:(CFTimeInterval)currentTime
+{
+    /* Called before each frame is rendered */
+    // Handle time delta.
+    //Prevents bad stuff happening
+    CFTimeInterval timeSinceLast = currentTime - self.lastUpdateTimeInterval;
+    self.lastUpdateTimeInterval = currentTime;
+    if (timeSinceLast > 1) { // more than a second since last update
+        timeSinceLast = 1.0 / 120.0;
+        self.lastUpdateTimeInterval = currentTime;
+    }
+    
+    [self updateWithTimeSinceLastUpdate:timeSinceLast];
+}
+
+NSDate *startTime;
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    /* Called when a touch begins */
+    [super touchesBegan:touches withEvent:event];
+    
+    //Starts Timer
+    startTime = [NSDate date];
+
+    UITouch *touch = [touches anyObject];
+    CGPoint location = [touch locationInNode:self];
+    SKNode *node = [self nodeAtPoint:location];
+    
+    //Pauses Scene
+    if ([node.name isEqualToString:@"pauseButton"])
+    {
+        NSLog(@"Pause button pressed");
+    }
+    
+}
+
+-(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    /* Called when a touch ends */
+    [super touchesEnded:touches withEvent:event];
+    
+    NSTimeInterval elapsedTime = [startTime timeIntervalSinceNow];
+    NSString *elapsedTimeString = [NSString stringWithFormat:@"Elapsed time: %f", elapsedTime];
+    NSLog(@"%@", elapsedTimeString);
+    
+    
+    for (UITouch *touch in touches)
+    {
+        //Gets location of touch
+        CGPoint location = [touch locationInNode:self];
+        NSLog(@"Touch Location X: %f \n Touch Location Y: %f", location.x, location.y);
+        
+        //Sets ground
+        if (location.y < 89)
+        {
+            location.y = 89;
+        }
+        
+        //Moves and animates player
+        //int velocity = elapsedTime * -3000;
+        int velocity = 480.0/1.0;
+        NSLog(@"Velocity: %i", velocity);
+        float realMoveDuration = self.size.width / velocity;
+        SKAction *actionMove = [SKAction moveTo:location duration:realMoveDuration];
+        [self.playerSprite runAction:[SKAction sequence:@[actionMove]]];
+        
+    }
+    NSLog(@"Touch ended");
+}
+
+- (void)redBall:(SKSpriteNode *)redBall didCollideWithPlayer:(SKSpriteNode *)playerSprite
+{
+    NSLog(@"Player died");
+    [redBall removeFromParent];
+    [playerSprite removeFromParent];
+    
+    SKTransition *reveal = [SKTransition crossFadeWithDuration:0.5];
+    SKScene *endGameScene = [[EndGameScene alloc] initWithSize:self.size gameEnded:YES];
+    [self.view presentScene:endGameScene transition: reveal];
+}
+
+- (void)didBeginContact:(SKPhysicsContact *)contact
+{
+    SKPhysicsBody *firstBody, *secondBody;
+    
+    if (contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask)
+    {
+        firstBody = contact.bodyA;
+        secondBody = contact.bodyB;
+    }
+    else
+    {
+        firstBody = contact.bodyB;
+        secondBody = contact.bodyA;
+    }
+    
+    if ((firstBody.categoryBitMask & redBallCategory) != 0 && (secondBody.categoryBitMask & playerCategory) != 0)
+    {
+        [self redBall:(SKSpriteNode *) firstBody.node didCollideWithPlayer:(SKSpriteNode *) secondBody.node];
+    }
+}
+
+@end
